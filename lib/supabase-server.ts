2a92c02d9b4203b16c1c78b323e24793f88d8c03
Thanasis Diamantis <nd.6@hotmail.com>
2025-12-20 18:29:46 +0200

    admin and influencer dashboards

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,10 +1,10 @@
 // lib/supabase-server.ts
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
 // Αυτή η συνάρτηση θα χρησιμοποιείται από τις Protected Server Components
 export function createSupabaseServerClient() {
-  const cookieStore = cookies()
+  const cookieStore = cookies(); // Καλούμε τη συνάρτηση cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -12,15 +12,15 @@ export function createSupabaseServerClient() {
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          return cookieStore.get(name)?.value; // Τώρα το cookieStore είναι σωστός τύπος
         },
         set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
+          cookieStore.set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options })
+          cookieStore.set({ name, value: '', ...options });
         },
       },
     }
-  )
+  );
 }
