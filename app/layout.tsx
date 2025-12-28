244175c8686294509135aebc91c65b534078507b
Thanasis Diamantis <nd.6@hotmail.com>
2025-12-28 11:25:56 +0200

    professional cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ["latin", "greek"],
+  variable: "--font-roboto",
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -130,7 +127,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} font-sans antialiased`}
       >
         {children}
       </body>
